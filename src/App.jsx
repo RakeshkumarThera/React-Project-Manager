e82283ba45f9d274d5e71eb53c15eb3c28a157b3
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-10-16 20:15:47 -0400

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
+import NewProject from "./componenets/NewProject";
+import NoProjectSelected from "./componenets/NoProjectselected";
+import ProjectSidebar from "./componenets/ProjectSidebar";
+
 function App() {
+
+  const[projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects:[]
+  })
+
+  function handlesStartAddProject(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectState.selectedProjectId === null){
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handlesStartAddProject}/>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8"> 
+      <ProjectSidebar  onStartAddProject={handlesStartAddProject}/>
+      {content}
+    </main>
   );
 }
 
