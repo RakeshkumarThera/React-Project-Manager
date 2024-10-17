305219be1832cbb23b4b61282bd37a784e325701
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-10-16 22:44:29 -0400

    project addition state management

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,26 @@ function App() {
     })
   }
 
+  function handleAddProject(projectData){
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectState);
+
   let content;
 
   if(projectState.selectedProjectId === null){
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject}/>
   } else if (projectState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handlesStartAddProject}/>
   }
