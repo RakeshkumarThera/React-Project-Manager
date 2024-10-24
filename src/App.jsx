7a2caaac3b1d97411333acc69bed5c296482d785
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-10-24 16:49:32 -0400

    Added functionality for save button

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,15 @@ function App() {
 
   function handleAddProject(projectData){
     setProjectState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId,
       }
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
@@ -46,7 +48,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8"> 
-      <ProjectSidebar  onStartAddProject={handlesStartAddProject}/>
+      <ProjectSidebar  onStartAddProject={handlesStartAddProject} projects={projectState.projects}/>
       {content}
     </main>
   );
